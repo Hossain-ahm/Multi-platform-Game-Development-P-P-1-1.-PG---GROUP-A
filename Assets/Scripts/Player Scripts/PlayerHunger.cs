643234c2a820c777565005d81df1bc24dcb33d78
Hossain Ahmed <hossainahmed2002@gmail.com>
2025-10-20 18:40:49 +0100

    added hunger decrease

--- Assets/Scripts/Player Scripts/PlayerHunger.cs
+++ Assets/Scripts/Player Scripts/PlayerHunger.cs
@@ -7,16 +7,24 @@ public class PlayerHunger : MonoBehaviour
 {
     public float hunger;
     public float maxHunger;
+	public float hungerDecrease; // amount the hunger goes down
+	public float hungerRate; // rate of the hunger decreases
+	public float hungerTime; 
     public Image hungerBar;
     // Start is called before the first frame update
     void Start()
     {
         maxHunger = hunger;
+		hungerTime = hungerRate;
     }
 
     // Update is called once per frame
     void Update()
     {
         hungerBar.fillAmount = Mathf.Clamp(hunger/maxHunger, 0, 1);
+		hungerTime -= Time.deltaTime;
+		if (!(hungerTime <= 0)) return;
+		hungerTime = hungerRate;
+		hunger -= hungerDecrease;
     }
 }
