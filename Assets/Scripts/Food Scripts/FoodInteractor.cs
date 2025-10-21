c092b6124e36f8fe80666429a51a02b2b4a2a56f
Hossain Ahmed <hossainahmed2002@gmail.com>
2025-10-21 01:46:03 +0100

    food has ability to interact

---
+++ Assets/Scripts/Food Scripts/FoodInteractor.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodInteractor : MonoBehaviour
+{
+    public void Interact()
+    {
+        Debug.Log("Interacting with food");
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerInteract.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float interactRange = 2f;
+            Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+            foreach (Collider col in colliderArray)
+            {
+                if (col.TryGetComponent(out FoodInteractor foodInteractor))
+                {
+                    foodInteractor.Interact();
+                }
+            }
+        }
+    }
+}
