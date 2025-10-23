28d3003e9ec07e0b14ec7a4f4fc592330c864888
savdotsaiko <koustubhwayfalkar11@gmail.com>
2025-10-23 02:02:40 +0100

    advanced bird controller + animations
    added the dive & fling up mechanic to the flight, fixed turning, and created custom flap & hover anims for the bird model.

--- Assets/BirdController.cs
+++ Assets/BirdController.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BirdController : MonoBehaviour
 {
-    public float flapUpForce = 8f;     // vertical lift from a flap
-    public float flapForwardForce = 5f; // extra forward thrust
-    public float liftForce = 4f;        // gentle lift when gliding
-    public float turnSpeed = 60f;    // degrees per second
+    public float flapUpForce = 8f;
+    public float flapForwardForce = 5f;
+    public float defaultLiftForce = 4f;
+    public float hoverLiftForce = 6f;
+    public float turnSpeed = 60f;
     public float bankAngle = 30f;
+    private float speedFactor = 0f;
+    float boostDuration, boostTimer;
+    [SerializeField] float rotationSpeed = 2f;
+    [SerializeField] float pitchAmount = 30f;
+    [SerializeField] float rollAmount = 45f;
+    [SerializeField] Animator birdAnimator;
+
     private Rigidbody rb;
-    bool flapQueued = false;
+    bool flapQueued = false, diveUp = false, isBoosting = false, hasDove = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -23,47 +32,92 @@ public class BirdController : MonoBehaviour
 
     void FixedUpdate()
     {
-        // lift
-        rb.AddForce(Vector3.up * liftForce, ForceMode.Acceleration);
+        //constant fwd and hover force
+        rb.AddForce(transform.forward * 5f, ForceMode.Acceleration);
+        rb.AddForce(Vector3.up * (hoverLiftForce), ForceMode.Acceleration);
 
-        // Forward input (holding W)
         float forwardInput = Input.GetAxis("Vertical");
         if (forwardInput > 0f)
+        {
             rb.AddForce(transform.forward * 10f, ForceMode.Acceleration);
-        else if (forwardInput == 0f)
-            rb.AddForce(transform.forward * 2f, ForceMode.Acceleration);
+            rb.AddForce(Vector3.down * (hoverLiftForce / 2), ForceMode.Acceleration);
+            hasDove = true;
+        }
 
+        //DIVING UP ON KEY PRESSED
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (!isBoosting && !diveUp && hasDove)
+            {
+                diveUp = true;
+                speedFactor = Mathf.Clamp(rb.velocity.magnitude, 0f, 100f);
+                boostDuration = Mathf.Min(speedFactor / 10f, 4f);
+                Debug.Log("UP" + boostDuration + " " + speedFactor);
+                boostTimer = 0f;
+                isBoosting = true; hasDove = false;
+            }
+        }
+        else
+        {
+            diveUp = false;
+            isBoosting = false;
+            boostTimer = 0f;
+        }
+        //ACTUAL DIVEUP FORCES APPLIED
+        if (diveUp && isBoosting)
+        {
+            boostTimer += Time.fixedDeltaTime;
+            float lift = defaultLiftForce * (speedFactor / 5f);
+            rb.AddForce(Vector3.up * lift, ForceMode.Acceleration);
+            rb.AddForce(transform.forward * speedFactor / 25f, ForceMode.Acceleration);
+            if (boostTimer >= boostDuration)
+            {
+                isBoosting = false;
+                diveUp = false;
+            }
+        }
+
+        //FLAP WINGS
         if (flapQueued)
         {
             rb.AddForce(Vector3.up * flapUpForce + transform.forward * flapForwardForce, ForceMode.Impulse);
+            birdAnimator.SetTrigger("flap");
             flapQueued = false;
         }
 
-        // Turning logic 
-        float horizontalInput = Input.GetAxis("Horizontal");
+        //TURNING LOGIC
+        float horizontal = Input.GetAxis("Horizontal");
 
-        if (Mathf.Abs(horizontalInput) > 0.01f)
-        {
-            float yawDelta = horizontalInput * turnSpeed * Time.fixedDeltaTime;
-            Quaternion yawRotation = Quaternion.Euler(0f, yawDelta, 0f);
-            rb.MoveRotation(rb.rotation * yawRotation);
-        }
+        float targetHRoll = -horizontal * bankAngle;
+        float targetVRoll = -horizontal * bankAngle;
 
-        // visual roll
-        float targetBank = -horizontalInput * bankAngle;
-        Vector3 euler = rb.rotation.eulerAngles;
-        float newZ = Mathf.LerpAngle(euler.z, targetBank, Time.fixedDeltaTime * 3f);
-        Quaternion finalRot = Quaternion.Euler(euler.x, euler.y, newZ);
-        rb.MoveRotation(finalRot);
+        Vector3 currentEuler = transform.localEulerAngles;
+        if (currentEuler.x > 180) currentEuler.x -= 360;
+        if (currentEuler.y > 180) currentEuler.y -= 360;
+        if (currentEuler.z > 180) currentEuler.z -= 360;
 
-        // rotate fwd
+        float newHRoll = Mathf.Lerp(currentEuler.z, targetHRoll, Time.fixedDeltaTime * 3f);
+        float newVRoll = Mathf.Lerp(currentEuler.x, targetVRoll, Time.fixedDeltaTime * 3f);
+
+        float yawTurnSpeed = Mathf.Abs(newHRoll / bankAngle) * turnSpeed;
+        float pitchTurnSpeed = Mathf.Abs(newVRoll / bankAngle) * turnSpeed;
+        float yawDirection = -Mathf.Sign(newHRoll);
+        float pitchDirection = -Mathf.Sign(newVRoll);
+        float newYaw = currentEuler.y + yawDirection * yawTurnSpeed * Time.fixedDeltaTime;
+
+        Quaternion targetRot = Quaternion.Euler(currentEuler.x, newYaw, newHRoll);
+        transform.rotation = targetRot;
+
+        //CHANGE VELOCITY TO MATCH ROTATION
         Vector3 currentVel = rb.velocity;
-        float vertical = currentVel.y;
+        float verticalVel = currentVel.y;
         float speed = new Vector3(currentVel.x, 0f, currentVel.z).magnitude;
 
-        Vector3 desiredVel = rb.transform.forward * speed;
-        Vector3 newVel = Vector3.Lerp(new Vector3(currentVel.x, 0f, currentVel.z), desiredVel, Time.fixedDeltaTime * 2f);
-        rb.velocity = new Vector3(newVel.x, vertical, newVel.z);
-
+        Vector3 desiredVel = transform.forward * speed;
+        rb.velocity = Vector3.Lerp(rb.velocity, desiredVel, Time.fixedDeltaTime * 2f);
+        rb.velocity = new Vector3(rb.velocity.x, verticalVel, rb.velocity.z);
     }
+
+
+
 }
