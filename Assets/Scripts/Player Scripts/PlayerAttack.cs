6bbec3b69b9102a1036d2e2145a142ef0c440284
Abheer05 <159877973+Abheer05@users.noreply.github.com>
2026-01-08 13:46:49 +0000

    save

--- Assets/Scripts/Player Scripts/PlayerAttack.cs
+++ Assets/Scripts/Player Scripts/PlayerAttack.cs
@@ -8,7 +8,6 @@ namespace Player_Scripts
         [SerializeField] private PlayerMana playerMana;
         [SerializeField] private GameObject sword;
         [SerializeField] private GameObject shield;
-        [SerializeField] private GameObject magic_ball;
 
         private bool isAttacking = false;
         private bool isDefending = false;
@@ -17,14 +16,11 @@ namespace Player_Scripts
         private float attackTime = 1f;
         private float counter2 = 0;
         private float defenseTime = 1f;
-        private float counter3 = 0;
-        private float magicTime = 1f;
-        
+
         private void Start()
         {
             sword.SetActive(false);
             shield.SetActive(false);
-            magic_ball.SetActive(false);
         }
 
         private void Update()
@@ -43,14 +39,6 @@ namespace Player_Scripts
                 counter2 += Time.deltaTime;
             }
 
-            if (Input.GetKeyDown(KeyCode.R) && !isMagic)
-            {
-                magic_ball.SetActive(true);
-                playerMana.useMana(30);
-                isMagic = true;
-                counter3 += Time.deltaTime;
-            }
-
             if (isAttacking)
             {
                 counter += Time.deltaTime;
@@ -58,10 +46,6 @@ namespace Player_Scripts
             if (isDefending)
             {
                 counter2 += Time.deltaTime;
-            }
-            if (isMagic)
-            {
-                counter3 += Time.deltaTime;
             }
 
             if (counter > attackTime)
@@ -76,12 +60,7 @@ namespace Player_Scripts
                 isDefending = false;
                 shield.SetActive(false);
             }
-            if (counter3 > magicTime)
-            {
-                counter3 = 0;
-                isMagic= false;
-                magic_ball.SetActive(false);
-            }
+
         }
         
         
